1bf30ca8789c84c561fa193c0da472eebf5c71ea
Juliana <Juliana@Juliana-PC>
2014-12-20 14:21:07 -0200

    fornecedor

---
+++ src/Model/Fornecedor.java
@@ -0,0 +1,18 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Fornecedor {
+	public static ArrayList<Fornecedor> lista = new ArrayList<Fornecedor>();
+	public String cod;
+	public String nome;
+	public String cnpj;
+	public String logradouro;
+	public String comp;
+	public String cep;
+	public String cidade;
+	public String uf;
+	public String email;
+	public String tel;
+	public String bairro;
+}
