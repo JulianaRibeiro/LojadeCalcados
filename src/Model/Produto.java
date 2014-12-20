2670fb18f98c2089b60504b1711b2b03f5ecdc30
Juliana <Juliana@Juliana-PC>
2014-12-20 14:23:07 -0200

    produto

---
+++ src/Model/Produto.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Produto {
+	public static ArrayList<Produto> lista = new ArrayList<Produto>();
+	public String cod;
+	public String nome;
+	public String qtd;
+	public String valorUn;
+	public String estTotal;
+	public String estMin;
+	public String desc;
+	
+
+}
