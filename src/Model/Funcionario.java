428626ca11f320938c99aaf636993605a178df93
Juliana <Juliana@Juliana-PC>
2014-12-20 14:22:51 -0200

    funcionário

---
+++ src/Model/Funcionario.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Funcionario {
+	public static ArrayList<Funcionario> lista = new ArrayList<Funcionario>();
+	public String cod;
+	public String nome;
+	public String cpf;
+	public String rg;
+	public String dtnasc;
+	public String cargo;	
+	public String genero;
+	public String ec;
+	public String logradouro;
+	public String comp;
+	public String cep;
+	public String cidade;
+	public String uf;
+	public String email;
+	public String tel;
+	public String bairro;
+}
