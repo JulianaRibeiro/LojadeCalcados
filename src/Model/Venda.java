17dcd072131ae537dfcb1ddef0405611310b1f26
Juliana <Juliana@Juliana-PC>
2014-12-20 14:23:23 -0200

    venda

---
+++ src/Model/Venda.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Venda {
+	public static ArrayList<Venda> lista = new ArrayList<Venda>();
+	public String cod;
+	public String nome;
+	public String qtd;
+	public String valorUn;
+	public String desc;
+	public String pag;
+	
+
+
+}
